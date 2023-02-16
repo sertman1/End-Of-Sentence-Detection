95a1aa439d80f3cefbfd235f211b478701ac19da
sertman1 <sertman1@jhu.edu>
2023-02-16 17:03:17 -0500

    New best: both scoring in the 0.80s now

--- hw1b.py
+++ hw1b.py
@@ -1,25 +1,53 @@
 import argparse
 from itertools import groupby
 
-from sklearn.tree import DecisionTreeClassifier
+# from sklearn.tree import DecisionTreeClassifier
+# from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 
+addressVocabulary = ['tel', 'fax', 'e-mail', 'email', 'p.o.', 'attn', 'phone']
 
 class SegmentClassifier:
     def train(self, trainX, trainY):
-        self.clf = DecisionTreeClassifier()  # TODO: experiment with different models
+        # self.clf = DecisionTreeClassifier()
+        # self.clf = svm.SVC()
+        self.clf = RandomForestClassifier()
         X = [self.extract_features(x) for x in trainX]
         self.clf.fit(X, trainY)
 
     def extract_features(self, text):
         words = text.split()
-        features = [  # TODO: add features here
+        features = [
             len(text),
             len(text.strip()),
             len(words),
             1 if '>' in words else 0,
             text.count(' '),
+            sum(1 if isWordFollowedByColon(w) else 0 for w in words),
             # sum(1 if w.isupper() else 0 for w in words)
+            computeRatioOfNumbersToWords(words),
+            totalNumberOfNumbers(words),
+            numberOfOneAndTwoLetterWords(words) / len(words),
+            text.count('@'),
+            text.count(':'),
+            text.count('-'),
+            sum(1 if w.lower() in addressVocabulary else 0 for w in words),
+            
+            # 624 / 722  0.8642659279778393
+            #63 / 78  0.8076923076923077
+
+            # sum(1 if w == 'From:' else 0 for w in words),
+            # sum(1 if w == 'Lines:' else 0 for w in words)
+            ### No noticeable improvements from the above features 
+
+
+            # From: pattern
+            # position relative to NNHEAD 
+            # detection for centering, indentation
+            # detector for length of block, number of line in block
+            # spacing in between words
+            # ratio of 1 to 2 letters to everything else
         ]
         return features
 
@@ -27,6 +55,37 @@ class SegmentClassifier:
         X = [self.extract_features(x) for x in testX]
         return self.clf.predict(X)
 
+def isWordFollowedByColon(word):
+    if ord(word[0]) >= 65 and ord(word[0]) <= 90 and word[len(word) - 1] == ':':
+        return 1
+    return 0
+
+def totalNumberOfNumbers(words):
+    numNumbers = 0
+    for w in words:
+        if any(char.isdigit for char in w):
+            numNumbers += 1
+    return numNumbers
+
+def computeRatioOfNumbersToWords(words):
+    numWords = 0
+    numNumbers = 0
+    for w in words:
+        if any(char.isdigit for char in w):
+            numNumbers += 1
+        else:
+            numWords += 1
+    if numWords == 0:
+        return numNumbers - 1
+    else:
+        return numNumbers / numWords
+
+def numberOfOneAndTwoLetterWords(words):
+    total = 0
+    for w in words:
+        if len(w) <= 2:
+            total += 1
+    return total
 
 def load_data(file):
     with open(file) as fin:
