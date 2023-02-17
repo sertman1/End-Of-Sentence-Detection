8bde7d1044af6007c5653caaca9a02656a149061
sertman1 <sertman1@jhu.edu>
2023-02-16 20:11:00 -0500

    final submit

--- hw1b.py
+++ hw1b.py
@@ -1,12 +1,12 @@
 import argparse
 from itertools import groupby
 
-# from sklearn.tree import DecisionTreeClassifier
-# from sklearn import svm
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 
-addressVocabulary = ['tel', 'fax', 'e-mail', 'email', 'p.o.', 'attn', 'phone']
+addressVocabulary = ['tel', 'fax', 'e-mail', 'email', 'p.o.', 'attn', 'phone', 'tel:', 'fax:', 'e-mail:', 'email:', 'p.o.:', 'attn:', 'phone:']
 
 class SegmentClassifier:
     def train(self, trainX, trainY):
@@ -18,42 +18,79 @@ class SegmentClassifier:
 
     def extract_features(self, text):
         words = text.split()
+        centerPosition = rightPositionOfFirstWord(text) - leftPositionOfLastWord(text) / 2 + leftPositionOfLastWord(text)
         features = [
             len(text),
             len(text.strip()),
             len(words),
             1 if '>' in words else 0,
             text.count(' '),
+            text.count('\n'), # length of block in lines
+            rightPositionOfFirstWord(text),
+            1 if abs(40 - centerPosition) <= 3 else 0, # is the text centered
+            text.count("--"),
             sum(1 if isWordFollowedByColon(w) else 0 for w in words),
-            # sum(1 if w.isupper() else 0 for w in words)
+            sum(1 if w.isupper() else 0 for w in words),
             computeRatioOfNumbersToWords(words),
             totalNumberOfNumbers(words),
             numberOfOneAndTwoLetterWords(words) / len(words),
             text.count('@'),
             text.count(':'),
             text.count('-'),
+            text.count('.'),
+            text.count('?'),
+            text.count('/'),
             sum(1 if w.lower() in addressVocabulary else 0 for w in words),
-            
+        
             # 624 / 722  0.8642659279778393
-            #63 / 78  0.8076923076923077
+            # 63 / 78  0.8076923076923077
 
-            # sum(1 if w == 'From:' else 0 for w in words),
-            # sum(1 if w == 'Lines:' else 0 for w in words)
-            ### No noticeable improvements from the above features 
+            sum(1 if w == 'From:' else 0 for w in words),
+            sum(1 if w == 'Lines:' else 0 for w in words),
+            sum (1 if w == 'Path:' else 0 for w in words),
 
-
-            # From: pattern
             # position relative to NNHEAD 
             # detection for centering, indentation
             # detector for length of block, number of line in block
             # spacing in between words
             # ratio of 1 to 2 letters to everything else
+            # sum(1 if isAreaCode(w) else 0 for w in words),
+
+            # length of block in lines
+            # position of the block from top/bottom
+            # is-text-centered?
+                # centerposition = rightpos-leftpos / 2 + leftpos
+                # absoffsetfrom40 = abs(40-centerposition)
+            # average / max line length in document
+            # indent-of-line (chars of whitespace before 1st char)
+
+            # % of ascii characters / (num lines * 80)
+            # %ofAlphaNumeric, %ofAsciiGraphicChars
+
+            sum(1 if char.isalnum() else 0 for char in text),
+
+            #ZIP CODE DETECTOR AND NUMBER DETECTOR
+            # Department, Institute, Division, Program, Committee, chair etc. on an indented block
         ]
         return features
 
     def classify(self, testX):
         X = [self.extract_features(x) for x in testX]
         return self.clf.predict(X)
+
+def rightPositionOfFirstWord(text):
+    i = 0
+    while text[i] == " ":
+        i += 1
+    return i
+
+def leftPositionOfLastWord(text):
+    i = rightPositionOfFirstWord(text)
+    while text[i] != " " or text[i] != "\n":
+        i += 1
+        if i == len(text):
+            break
+    return i
 
 def isWordFollowedByColon(word):
     if ord(word[0]) >= 65 and ord(word[0]) <= 90 and word[len(word) - 1] == ':':
@@ -86,6 +123,11 @@ def numberOfOneAndTwoLetterWords(words):
         if len(w) <= 2:
             total += 1
     return total
+
+def isAreaCode(word):
+    if len(word) == 5 and word[0] == '(' and word[4] == ')' and word[2].isdigit() and word[3].isdigit() and word[4].isdigit():
+        return 1
+
 
 def load_data(file):
     with open(file) as fin:
